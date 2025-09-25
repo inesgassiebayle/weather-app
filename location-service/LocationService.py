0b12b23f261506281766619d2b9a68d6f6999941
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2025-09-25 17:18:16 -0300

    location service

--- location-service/LocationService.py
+++ location-service/LocationService.py
@@ -1,12 +1,19 @@
 import requests
-
-class DummyRequest:
-    def __init__(self, ip):
-        self.ip = ip
+import os
+import grpc
+from concurrent import futures
 
 class LocationService:
-    def getLocation(self, request):
+    def getLocation(self, request: L):
         ip = request.ip
         response = requests.get(f"https://ipwho.is/{ip}?fields=latitude,longitude", timeout=5)
         location = response.json()
         return location
+
+def serve():
+    port = os.getenv("LOCATION_PORT")
+    locationServer = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # weatherapp_pb2_grpc.add_LocationServiceServicer_to_server(LocationService(), locationServer)
+    locationServer.add_insecure_port(f"[::]:{port}")
+    locationServer.start()
+    print(f"LocationService escuchando en :{port}")
